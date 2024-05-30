8a580dd85093fa04fef9ed64709ecd44eb2658f7
grigoriy.pronin <g.pronin@labirint-t.ru>
2024-05-30 13:41:35 +0300

    Пример c ошибкой сервера (500)

---
+++ lesson_2/app_10.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, abort
+
+# from lesson_2.db import get_blog # Специально сломан импорт
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/blog/<int:id>/')
+def get_blog_by_id(id):
+    ...  # Запрос для поиска блога по id
+    result = get_blog(id)
+    if result is None:
+        abort(404)
+    ...  # Возврат найденного блога
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    app.logger.warning(e)
+    context = {
+        'title': 'Страница не найдена',
+        'url': request.base_url,
+    }
+    return render_template('500.html', **context), 500
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(debug=False)
