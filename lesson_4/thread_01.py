d8dd924ea8f421b1349b5b3bae07e6bc1d11a622
grigoriy.pronin <78648781+It-Makes-No-Sense@users.noreply.github.com>
2024-06-07 16:43:18 +0300

    Пример Многопоточного кода

---
+++ lesson_4/thread_01.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+counter = 0
+
+
+def increment():
+    global counter
+    for _ in range(1_000_000):
+        counter += 1
+    print(f"Значение счетчика: {counter}")
+
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=increment)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print(f'Финальное значение счетчика: {counter}')
